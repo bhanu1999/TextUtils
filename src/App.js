cdd5b445148d70aa7a27d0455693eeddcdc8b9f2
bhanu1999 <sakinalabhanu99@gmail.com>
2022-04-18 10:50:50 +0530

    starting basic app creation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+let name="Bhanu Prakash"
 function App() {
-  return (
+  const element_to_return=(
     <>
-    <h1>Hello world
-      
+    <h1>Hello ,{name+"VIT"}
+
+    </h1>
+    <h1 >
+      hi there
     </h1>
     </>
   );
+  return element_to_return; 
 }
 
 export default App;
