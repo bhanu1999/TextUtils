dc3df669ce393be38029fc0e521510f90da70ff6
bhanu1999 <sakinalabhanu99@gmail.com>
2022-04-20 10:45:22 +0530

    #9_exercise enhancing textUtil app

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,52 +1,88 @@
-import React,{useState} from 'react'
-
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-
-  const handleUpClick= ()=>{
+  const handleUpClick = () => {
     console.log("button clicked");
-    setText(text.toUpperCase(text))
-  }
-  const handleLowerClick= ()=>{
+    setText(text.toUpperCase(text));
+  };
+  const handleLowerClick = () => {
     console.log("button clicked");
-    setText(text.toLowerCase(text))
-  }
-  const handleClearClick=()=>{
+    setText(text.toLowerCase(text));
+  };
+  const handleClearClick = () => {
     setText("");
-  }
-  const textHandler= (event)=>{
-    console.log("button clicked");
+  };
+  // const handleSpaceClick = () => {
+    
+  //   setText(text.trim());
+  // };
+  const textHandler = (event) => {
+    // console.log("button clicked");
     setText(event.target.value);
+  };
+  const speak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
   }
-  const [text, setText] = useState("")
+  const capitalFirstLetter = ()=>{
+    let words = text.split(" ")
+   let uppercaseword = ' '
+    words.forEach(element => {
+       uppercaseword += element.charAt(0).toUpperCase() + element.slice(1) + " "
+    });
+    setText(uppercaseword)
+}
+  const [text, setText] = useState("");
 
   return (
     <>
-    <h1>{props.title}</h1>
+      <h1>{props.title}</h1>
       <div className=" container mb-3">
-       
-        <textarea 
+        <textarea
           className="form-control"
           id="mybox"
           rows="8"
           value={text}
           onChange={textHandler}
-        >
-        
-
-        </textarea>
+        ></textarea>
         <br></br>
-        <button className="btn btn-primary mx-2" onClick={handleUpClick}> Convert to Upper Case</button>
-        <button className="btn btn-success mx-2" onClick={handleLowerClick}> Convert to Lower Case</button>
-        <button className="btn btn-primary mx-2" onClick={handleClearClick}> Clear Box</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          {" "}
+          Convert to Upper Case
+        </button>
+        <button className="btn btn-success mx-2" onClick={handleLowerClick}>
+          {" "}
+          Convert to Lower Case
+        </button>
+        <button className="btn btn-success mx-2" onClick={capitalFirstLetter}>
+          {" "}
+        Title Case
+        </button>
+        <button className="btn btn-success mx-2" onClick={speak}>
+          {" "}
+        Speak
+        </button>
         
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+          {" "}
+          Clear Box
+        </button>
       </div>
-      <div className="container my-2" >
+      <div className="container my-2">
         <h1>Text Summary</h1>
-        <i><p>Words are {text.split(" ").length}  and characters are   {text.length}</p></i>
-       <b> <p>Time to Read:{text.split(" ").length*0.008}mins</p></b>
+        <i>
+          <p>
+            Words are {text.split(" ").length} and characters are {text.length}
+          </p>
+        </i>
+        <b>
+          {" "}
+          <p>Time to Read:{text.split(" ").length * 0.008}mins</p>
+        </b>
         <h2>Preview</h2>
         <p>{text}</p>
+        <h2>Frequencies</h2>
       </div>
     </>
   );
